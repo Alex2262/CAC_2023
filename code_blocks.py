b3bda1eb714c7b0ccc3a9cc83221f946499f4c44
alexandertian <alex2262alexander@gmail.com>
2023-10-28 17:56:49 -0400

    changes

--- code_blocks.py
+++ code_blocks.py
@@ -3,19 +3,71 @@
 from objects import *
 
 
-class CodeBlock(RectTextObject):
+class BackgroundDot(RectObject):
+    def __init__(self, position):
+        self.real_x = position[0]
+        self.real_y = position[1]
+        super().__init__(LAYER_COLORS[2], (position[0], position[1], 1, 1), 0, 0)
+
+    def shift(self, deltas):
+        self.x = self.real_x + deltas[0] % 20
+        self.y = self.real_y + deltas[1] % 20
+
+
+class CodeBlock(RectTextButton):
     def __init__(self, color, rect, is_template, text='', text_color=(216, 222, 233), text_size=20):
-        super().__init__(color, rect, 0, 8, text, text_color, text_size)
+        super().__init__(color, rect, 0, 2, "codeblock:empty", text, text_color, text_size)
+
+        self.real_x = self.x  # top left
+        self.real_y = self.y
 
         self.is_template = is_template
         self.string_code = ""
         self.parent = None
-        self.child = None
+        self.child = None  # blocks that are attached to bottom
+        # self.nested_children = []
+
+    def hold(self, mouse_pos):
+        new_x = mouse_pos[0] - self.width // 2
+        new_y = mouse_pos[1] - self.height // 2
+
+        # Change the real position when the mouse is moving it
+        self.real_x += new_x - self.x
+        self.real_y += new_y - self.y
+
+        self.x = new_x
+        self.y = new_y
+
+    def shift(self, deltas):  # scrolling
+        self.x = self.real_x + deltas[0]
+        self.y = self.real_y + deltas[1]
+
+    def assign_parent(self, parent_block):
+        if parent_block.child is not None:
+            pass
+        if parent_block is None:
+            print("Oh no no parent")
+            if self.parent is not None:
+                self.parent.child = None
+                self.parent = None
+        else:
+            print("HI")
+            print(parent_block.real_x, parent_block.real_y)
+            self.parent = parent_block  # this oop does not copy the object right
+            self.real_x = parent_block.real_x
+            self.real_y = parent_block.real_y + parent_block.height
+
+            self.x = parent_block.x
+            self.y = parent_block.y + parent_block.height
+            parent_block.child = self
 
 
 class DrillForwards(CodeBlock):
-    def __init__(self, is_template):
-        super().__init__((0, 0, 255), is_template, "Drill Forwards")
+    def __init__(self, position, is_template):
+        self.width  = 200
+        self.height = 30
+        super().__init__((0, 0, 255),
+                         (position[0], position[1], self.width, self.height), is_template, "Drill Forwards")
 
         '''
         Moves the drill forwards in the current direction and removes the block in its new position
@@ -31,18 +83,38 @@ class DrillForwards(CodeBlock):
         """
 
 
-class Turn(CodeBlock):
-    def __init__(self, is_template):
-        super().__init__((0, 0, 255), is_template, "Drill Forwards")
+class TurnLeft(CodeBlock):
+    def __init__(self, position, is_template):
+        self.width  = 200
+        self.height = 30
+        super().__init__((0, 0, 255), (position[0], position[1], self.width, self.height), is_template, "Turn Left")
 
         '''
-        Moves the drill forwards in the current direction and removes the block in its new position
+        Turns the Drill Left (-90°)
+        Rotation: Direction(x, y) -> Direction(-y, x)
         '''
 
         self.string_code = """
 
-        if self.direction[0] == 0:
+        (self.direction[0], self.direction[1]) = (-self.direction[1], self.direction[0])
             
+        """
+
+
+class TurnRight(CodeBlock):
+    def __init__(self, position, is_template):
+        self.width  = 200
+        self.height = 30
+        super().__init__((0, 0, 255), (position[0], position[1], self.width, self.height), is_template, "Turn Right")
+
+        '''
+        Turns the Drill Right (90°)
+        Rotation: Direction(x, y) -> Direction(y, -x)
+        '''
+
+        self.string_code = """
+
+        (self.direction[0], self.direction[1]) = (self.direction[1], -self.direction[0])
 
         """
 
--- code_screen.py
+++ code_screen.py
@@ -1,5 +1,8 @@
 
-from objects import *
+from code_blocks import *
+
+CODE_BLOCK_TEMPLATES_MARGIN = 230
+RIGHT_SIDE_BAR_MARGIN = SCREEN_WIDTH - 200
 
 
 def code_screen(screen):
@@ -8,10 +11,24 @@ def code_screen(screen):
     # Used to determine which objects are selected
     selected_object = None
 
+    first_mouse_pos = (0, 0)
+    last_canvas_delta = (0, 0)
+    canvas_delta = (0, 0)
+    scrolling = False
+
+    # Used for creating the template code blocks
+    all_code_blocks = [
+        DrillForwards,
+        TurnLeft,
+        TurnRight
+    ]
+
+    background_dots = []
+
     basic_objects = [
-        RectObject((197, 203, 214), (0, 0, 200, SCREEN_HEIGHT), 0, 0),
-        RectObject((197, 203, 214), (SCREEN_WIDTH - 200, 0, 200, SCREEN_HEIGHT), 0, 0),
-        RectObject((255, 203, 214), (0, 0, 50, 50), 0, 0),
+        RectObject((197, 203, 214), (0, 0, CODE_BLOCK_TEMPLATES_MARGIN, SCREEN_HEIGHT), 0, 0),
+        RectObject((197, 203, 214), (RIGHT_SIDE_BAR_MARGIN, 0, 200, SCREEN_HEIGHT), 0, 0),
+        DrillForwards((10, 10, 180, 30), True),
     ]
 
     buttons = [
@@ -21,7 +38,24 @@ def code_screen(screen):
                        "menu:2", "Drill", (197, 203, 214), 30),
         RectTextButton((150, 157, 171), (SCREEN_WIDTH - 190, 120, 180, 40), 0, 0,
                        "menu:3", "Tips", (197, 203, 214), 30),
+        RectTextButton((150, 157, 171), (SCREEN_WIDTH - 190, 500, 180, 40), 0, 0,
+                       "zoom:center", "Center", (197, 203, 214), 30),
     ]
+
+    real_code_blocks = []
+
+    # Create the background dots
+    for b_x in range(CODE_BLOCK_TEMPLATES_MARGIN, RIGHT_SIDE_BAR_MARGIN, 20):
+        for b_y in range(0, SCREEN_HEIGHT, 20):
+            background_dots.append(BackgroundDot((b_x, b_y)))
+
+    # Create the template code blocks
+    code_block_template_y = 10
+    for code_block in all_code_blocks:
+        buttons.append(code_block((10, code_block_template_y), True))
+        code_block_template_y += basic_objects[-1].height + 10  # +10 for the space in between each code block
+
+    current_code_object = None
 
     # ----------------- The Main GUI Loop -----------------
     running = True
@@ -35,10 +69,50 @@ def code_screen(screen):
                 running = False
                 break
 
+            # ----------------- Mouse Clicked -----------------
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                selected_object = get_selected_object(mouse_pos, buttons + real_code_blocks)
+
+                if selected_object is None:
+                    if CODE_BLOCK_TEMPLATES_MARGIN < mouse_pos[0] < RIGHT_SIDE_BAR_MARGIN:
+                        first_mouse_pos = mouse_pos
+                        scrolling = True
+                    continue
+
+                actions = selected_object.action.split(":")
+
+                if actions[0] == "codeblock":
+
+                    if selected_object.is_template:
+                        real_code_blocks.append(type(selected_object)((10, selected_object.real_y), False))
+                        current_code_object = real_code_blocks[-1]
+                    elif CODE_BLOCK_TEMPLATES_MARGIN < mouse_pos[0] < RIGHT_SIDE_BAR_MARGIN:
+                        current_code_object = selected_object
+
             # ----------------- Mouse Released -----------------
             if event.type == pygame.MOUSEBUTTONUP:
                 mouse_pos = pygame.mouse.get_pos()
-                selected_object = get_selected_object(mouse_pos, buttons)
+                selected_object = get_selected_object(mouse_pos, buttons + real_code_blocks)
+
+                if current_code_object is not None:
+                    # block held over the deletion area
+                    if mouse_pos[0] < CODE_BLOCK_TEMPLATES_MARGIN:
+                        real_code_blocks.remove(current_code_object)
+
+                    # block is held near the bottom of another block
+                    adjacent_block = find_adjacent_block(current_code_object, real_code_blocks)
+                    if adjacent_block is not None:
+                        current_code_object.assign_parent(adjacent_block)
+                    else:
+                        # Map the real x and y accordingly
+                        current_code_object.real_x -= canvas_delta[0]
+                        current_code_object.real_y -= canvas_delta[1]
+
+                current_code_object = None  # Have stopped holding any code blocks
+
+                scrolling = False
+                last_canvas_delta = canvas_delta
 
                 if selected_object is None:
                     continue
@@ -50,12 +124,38 @@ def code_screen(screen):
                     if actions[1] != MAIN_MENU:
                         return int(actions[1])
 
+                if actions[0] == "zoom":
+                    if actions[1] == "center":
+                        last_canvas_delta = (0, 0)
+                        canvas_delta = (0, 0)
+                        shift_code_blocks(real_code_blocks, canvas_delta)
+                        shift_background_dots(background_dots, canvas_delta)
+
         mouse_pos = pygame.mouse.get_pos()
         selected_object = get_selected_object(mouse_pos, buttons)
 
+        # Shifting / Scrolling
+        if scrolling:
+            deltas = (mouse_pos[0] - first_mouse_pos[0], mouse_pos[1] - first_mouse_pos[1])
+
+            canvas_delta = (last_canvas_delta[0] + deltas[0], last_canvas_delta[1] + deltas[1])
+            shift_code_blocks(real_code_blocks, canvas_delta)
+            shift_background_dots(background_dots, canvas_delta)
+
+        # Holding
+        if current_code_object is not None:
+            current_code_object.hold(mouse_pos)
+
         screen.fill(MENU_SCREEN_COLOR)
+
+        draw_basic_objects(screen, background_dots)
+        draw_buttons(screen, selected_object, real_code_blocks)
         draw_basic_objects(screen, basic_objects)
         draw_buttons(screen, selected_object, buttons)
+
+        # Redraw the current code object on the top
+        if current_code_object is not None:
+            draw_buttons(screen, selected_object, [current_code_object])
 
         pygame.display.update()
 
@@ -70,6 +170,16 @@ def get_selected_object(mouse_pos, buttons):
             return button
 
     return None
+
+
+def shift_code_blocks(code_blocks, deltas):
+    for code_block in code_blocks:
+        code_block.shift(deltas)
+
+
+def shift_background_dots(background_dots, deltas):
+    for background_dot in background_dots:
+        background_dot.shift(deltas)
 
 
 def draw_basic_objects(screen, basic_objects):
